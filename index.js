2c307fc6fdb01a8a5158a7fe578e27bb1bbf80da
Elsen01 <elsen@gmail.com>
2023-01-25 00:26:19 +0400

    database-connect

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from "mongoose";
+mongoose.set('strictQuery', true);
+
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0.s5yqh.mongodb.net/?retryWrites=true&w=majority')
+    .then(()=>console.log('DB Ok'))
+    .catch((err) => console.log('DB ERROR',err))
 
 const app = express();
 
+app.use(express.json())
+
 app.get('/',(req,res) => {
     res.send('Hello world')
+})
+
+app.post('/auth/login',(req,res) => {
+    console.log(req.body)
+
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: 'Elsen askerov'
+    },'secret123',
+        )
+
+
+    res.json({
+        success: true,
+        token,
+    })
 })
 
 
